c63d8e078f2087357bd1f6456724222a83f06afb
Eric Bower <me@erock.io>
2022-11-29 13:30:47 -0500

    edge

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export const config = {
+  runtime: 'experimental-edge',
 }
+
+export default (req) => new Response('Hello world!')
