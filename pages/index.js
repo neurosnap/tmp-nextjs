64c56cca30eaf80015d9286bd139b23428ac2f0c
Eric Bower <me@erock.io>
2022-11-29 13:15:34 -0500

    changes

--- pages/index.js
+++ pages/index.js
@@ -61,7 +61,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           Powered by{' '}
-          Aptible
+          Aptiblesdasdsadas
         </a>
       </footer>
     </div>
